f627615b4bbe4c73a746cae1c0c27edde8316ca5
minami murata <minami_murata@outlook.com>
2023-07-17 21:13:41 +0900

    add blank for select box, and close #13

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -3,5 +3,5 @@ class Blog < ApplicationRecord
   belongs_to :cat, optional: true
   validates :title, presence: true
   validates :content, presence: true
-  enum age_range: { default: 0, under_one: 1, one_to_three: 2, four_to_six: 3, seven_to_nine: 4, ten_to_twelve: 5, over_thirteen: 6 }
+  enum age_range: { under_one: 1, one_to_three: 2, four_to_six: 3, seven_to_nine: 4, ten_to_twelve: 5, over_thirteen: 6 }
 end
